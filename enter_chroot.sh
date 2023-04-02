c88a490cd5aec6706833bb95a06e9df83a3fba5b
Jun Sun <jsun@junsun.net>
2023-04-02 15:43:22 -0700

    more reliable way to enter/exit chroot by recursively mount/umount /dev

--- enter_chroot.sh
+++ enter_chroot.sh
@@ -3,25 +3,19 @@
 set -u                  # treat unset variable as error
 #set -x
 
-sudo mount --bind /dev chroot/dev
+sudo mount --rbind /dev chroot/dev
+sudo mount --make-rslave chroot/dev
 sudo mount --bind /run chroot/run
+sudo mount --bind /proc chroot/proc
+sudo mount --bind /sys chroot/sys
 
 sudo ln -f chroot_build.sh chroot/root/chroot_build.sh
 
-sudo chroot chroot mount none -t proc /proc
-sudo chroot chroot mount none -t sysfs /sys
-sudo chroot chroot mount none -t devpts /dev/pts
-
 sudo chroot chroot 
-
-sudo chroot chroot umount /proc
-sudo chroot chroot umount /sys
-sudo chroot chroot umount /dev/pts
 
 sudo rm -f chroot/root/chroot_build.sh
 
-sleep 2
-
-sudo umount chroot/dev
+sudo umount chroot/sys
+sudo umount chroot/proc
 sudo umount chroot/run
-
+sudo umount -R chroot/dev
